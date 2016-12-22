cdb3a52befba5f9e0628e8806b4bc3a935b91ce1
Jakub Adamczyk <adamczyk777@gmail.com>
2016-12-22 17:06:02 +0100

    a bit of refactoring

--- breakout.py
+++ breakout.py
@@ -1,238 +1,194 @@
 import math
 import pygame
 
-# Define some colors
-black = (0, 0, 0)
+# global hardcoded variables for easier use of colors:
 white = (255, 255, 255)
+black = (0, 0, 0)
 blue = (0, 0, 255)
+pink = (255, 105, 180)
+red = (255, 0, 0)
+# hardcoded global block dimensions:
 block_width = 58
 block_height = 20
 
 
 class Block(pygame.sprite.Sprite):
-    """This class represents each block that will get knocked out by the ball
-    It derives from the "Sprite" class in Pygame """
+    """Class represents Block, element of the game that is intended to be destroyed by ball"""
 
     def __init__(self, color, x, y):
-        """ Constructor. Pass in the color of the block,
-            and its x and y position. """
-
-        # Call the parent class (Sprite) constructor
+        """ Constructor requires color, and coordinates """
         super().__init__()
-
-        # Create the image of the block of appropriate size
-        # The width and height are sent as a list for the first parameter.
+        # creates Blocks surface
         self.image = pygame.Surface([block_width, block_height])
-
-        # Fill the image with the appropriate color
+        # fill block with color
         self.image.fill(color)
-
-        # Fetch the rectangle object that has the dimensions of the image
+        # make object a rectangle with dimensions of image
         self.rect = self.image.get_rect()
-
-        # Move the top left of the rectangle to x,y.
-        # This is where our block will appear..
+        # place object where it should be placed
         self.rect.x = x
         self.rect.y = y
 
 
 class Ball(pygame.sprite.Sprite):
-    """ This class represents the ball
-        It derives from the "Sprite" class in Pygame """
+    """Class represents Ball, our main item that is bounced with paddle, used to destroy blocks"""
 
-    # Speed in pixels per cycle
+    # some class wide variables name says for itself
     speed = 10.0
-
-    # Floating point representation of where the ball is
     x = 0.0
-    y = 180.0
-
-    # Direction of ball (in degrees)
-    direction = 0
-
-    isActive = False
-
+    y = 0.0
+    direction = 0 # in degrees
+    isActive = False # determines whether ball is active (moving) at start of game
     width = 10
     height = 10
 
-    # Constructor. Pass in the color of the block, and its x and y position
     def __init__(self):
-        # Call the parent class (Sprite) constructor
         super().__init__()
-
-        # Create the image of the ball
+        # creating surface of the ball
         self.image = pygame.Surface([self.width, self.height])
-
-        # Color the ball
-        self.image.fill(white)
-
-        # Get a rectangle object that shows where our image is
+        # fill surface with some color
+        self.image.fill(red)
+        # Make proper rectangle that is filled with our surface and color
         self.rect = self.image.get_rect()
-
-        # Get attributes for the height/width of the screen
+        # storing class-wide our display surface dimesnions
         self.screenheight = pygame.display.get_surface().get_height()
         self.screenwidth = pygame.display.get_surface().get_width()
 
     def bounce(self, diff):
-        """ This function will bounce the ball
-            off a horizontal surface (not a vertical one) """
-
+        """function makes possible for the ball to bounce"""
         self.direction = (180 - self.direction) % 360
         self.direction -= diff
 
     def update(self):
-        """ Update the position of the ball. """
+        """function that updates position of ball
+        i have a condition isActive that determines whether we want the ball to behave statically
+        ie. when it is above paddle, or actively, so it actually moves, bounces etc. + returns True when we hit bottom,
+        so we use the function to determine life loss or game over state"""
         if self.isActive:
-            # Sine and Cosine work in degrees, so we have to convert them
+            # converting degrees to radians in order to use it for sine, and cosine
             direction_radians = math.radians(self.direction)
-
-            # Change the position (x and y) according to the speed and direction
+            # updating coords with actual speed calculated based on direction
             self.x += self.speed * math.sin(direction_radians)
             self.y -= self.speed * math.cos(direction_radians)
-
-            # Move the image to where our x and y are
+            # moving image accordingly to our coords
             self.rect.x = self.x
             self.rect.y = self.y
-
-            # Do we bounce off the top of the screen?
+            # determining top wall bounce
             if self.y <= 0:
-                self.bounce(0)
+                self.bounce(0) # diff is equal to 0
                 self.y = 1
-
-            # Do we bounce off the left of the screen?
+            # determining left wall bounce
             if self.x <= 0:
                 self.direction = (360 - self.direction) % 360
                 self.x = 1
-
-            # Do we bounce of the right side of the screen?
+            # determining right wall bounce
             if self.x > self.screenwidth - self.width:
                 self.direction = (360 - self.direction) % 360
                 self.x = self.screenwidth - self.width - 1
-
-            # Did we fall off the bottom edge of the screen?
+            # checking whether we did hit botto m not the paddle
             if self.y > 600:
                 return True
             else:
                 return False
         else:
+            # Get actual mouse position
             pos = pygame.mouse.get_pos()
             self.x = pos[0] + 50 - self.width/2
-            self.y = self.screenheight - 40
-
+            self.y = self.screenheight - 30
+            # Checking if our ball doesn't go to far right
             if self.x > self.screenwidth - 50 - self.width/2:
                 self.x = self. screenwidth - 50 - self.width/2
-
+            # Giving the object actual coordinates
             self.rect.x = self.x
             self.rect.y = self.y
 
 
-class Player(pygame.sprite.Sprite):
-    """ This class represents the bar at the bottom that the
-    player controls. """
+class Paddle(pygame.sprite.Sprite):
+    """Class represents paddle that we use to bounce the ball in block direction"""
 
     def __init__(self):
-        """ Constructor for Player. """
-        # Call the parent's constructor
+        """Constructor for paddle"""
         super().__init__()
-
+        # Define objects attributes
         self.width = 100
         self.height = 15
+        # set surface of certain size
         self.image = pygame.Surface([self.width, self.height])
+        # Fill it with color
         self.image.fill(white)
-
-        # Make our top-left corner the passed-in location.
+        # Fetch rectangle on the coloured surface
         self.rect = self.image.get_rect()
+        # Getting our main surface/background dimensions
         self.screenheight = pygame.display.get_surface().get_height()
         self.screenwidth = pygame.display.get_surface().get_width()
-
+        # Setting default position for paddle it needs some
         self.rect.x = 0
         self.rect.y = self.screenheight - self.height
 
     def update(self):
-        """ Update the player position. """
-        # Get where the mouse is
+        """Updating paddles position"""
+        # Getting mouse location
         pos = pygame.mouse.get_pos()
-        # Set the left side of the player bar to the mouse position
+        # setting x coord to be paddle position
         self.rect.x = pos[0]
-        # Make sure we don't push the player paddle
-        # off the right side of the screen
+        # Check whether the paddle doesn't go too far right
         if self.rect.x > self.screenwidth - self.width:
             self.rect.x = self.screenwidth - self.width
 
-
-# Call this function so the Pygame library can initialize itself
+"""Here starts our game logic"""
 pygame.init()
-
-# Declare variables for labels that show points and lives
+# Points and lives variable that is diplayed int op corners
 points = 0
-
 lives = 3
-
-# Create an 800x600 sized screen
+#declaring window dimensions
 screen = pygame.display.set_mode([800, 600])
-
-# Set the title of the window
-pygame.display.set_caption('Arkanoid')
-
-# Enable this to make the mouse disappear when over our window
+# Window label is being set to game name
+pygame.display.set_caption('Arkanoid by Jakub Adamczyk')
+# making mouse disappear when it is hovering over game window
 pygame.mouse.set_visible(0)
-
-# This is a font we use to draw text on the screen (size 36)
+# declaring main game font + its size
 font = pygame.font.Font(None, 36)
-
-# Create a surface we can draw on
+# create background/surface that all objects appear on
 background = pygame.Surface(screen.get_size())
-
-# Create sprite lists
+# our objects containers are being declared here
 blocks = pygame.sprite.Group()
 balls = pygame.sprite.Group()
 allsprites = pygame.sprite.Group()
-
-# Create the player paddle object
-player = Player()
+# creating paddle object and adding it to allsprites group
+player = Paddle()
 allsprites.add(player)
-
-# Create the ball
+# creating the ball object and adding it to balls group and allsprites group
 ball = Ball()
 allsprites.add(ball)
 balls.add(ball)
-
-# The top of the block (y position)
+# determining how far from top blocks are starting to generate
 top = 80
-
-# Number of blocks to create
+# determining number of blocks in a row
 blockcount = 12
-
-# --- Create block
 # Five rows of blocks
 for row in range(4):
-    # 32 columns of blocks
+    # 12 columns of blocks
     for column in range(0, blockcount):
-        # Create a block (color,x,y)
-        block = Block(blue, column * (block_width + 5) + 22, top)
+        # 2 different colors of blocks
+        if row % 2 == 0:
+            block = Block(pink, column * (block_width + 5) + 22, top)
+        else:
+            block = Block(blue, column * (block_width + 5) + 22, top)
         blocks.add(block)
         allsprites.add(block)
     # Move the top of the next row down
     top += block_height + 5
-
 # Clock to limit speed
 clock = pygame.time.Clock()
-
-# Is the game over?
+# game over state variable
 game_over = False
-
-# Exit the program?
-exit_program = False
-
-# Main program loop
-while not exit_program:
-
-    # Limit to 30 fps
-    clock.tick(30)
-
-    # Clear the screen
+# program close variable temrinates main game  if true
+exit_game = False
+# Main game loop
+while not exit_game:
+    # Limit to 45 fps also determines speed of game
+    clock.tick(45)
+    # screen refresh (clearing it)
     screen.fill(black)
-
     # Prints label with points, blocks that we destroyed
     labelPoints = font.render("Points: "+str(points), 1, white)
     screen.blit(labelPoints, (5, 5))
@@ -244,7 +200,7 @@ while not exit_program:
     # Process the events in the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exit_program = True
+            exit_game = True
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 ball.isActive = True
@@ -253,6 +209,27 @@ while not exit_program:
                 points = 0
                 lives = 3
                 game_over = False
+                allsprites.empty()
+                balls.empty()
+                blocks.empty()
+                player = Paddle()
+                allsprites.add(player)
+                ball = Ball()
+                allsprites.add(ball)
+                balls.add(ball)
+                top = 80
+                for row in range(4):
+                    for column in range(0, blockcount):
+                        # Create a block (color,x,y)
+                        if row % 2 == 0:
+                            block = Block(pink, column * (block_width + 5) + 22, top)
+                        else:
+                            block = Block(blue, column * (block_width + 5) + 22, top)
+                        blocks.add(block)
+                        allsprites.add(block)
+                    # Move the top of the next row down
+                    top += block_height + 5
+
 
     # Update the ball and player position as long
     # as the game is not over.
@@ -268,7 +245,10 @@ while not exit_program:
 
     # If we are done, print game over
     if game_over:
-        text = font.render("Game Over", True, white)
+        if points == 48:
+            text = font.render("YOU WON!", True, white)
+        else:
+            text = font.render("Game Over!", True, white)
         text_position = text.get_rect(centerx=background.get_width() / 2)
         text_position.top = 300
         screen.blit(text, text_position)
